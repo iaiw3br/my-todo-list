2ee18d5048316f523c500ab6eedba9f6b6fba062
Артем Иванов <iaiw3br@gmail.com>
2021-02-17 22:21:48 +0300

    feat: add initial files

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+
+import TodoList from "@/components/TodoList";
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  }
+}
+</script>
