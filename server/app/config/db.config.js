bd430315d9bfa4761ec891597962686971b3f950
Артем Иванов <iaiw3br@gmail.com>
2021-02-18 19:08:02 +0300

    feat: add db config settings

---
+++ server/app/config/db.config.js
@@ -0,0 +1,17 @@
+const {
+    HOST,
+    USER,
+    PASSWORD,
+    DB,
+    dialect,
+    pool
+} = require('./todo-config')
+
+module.exports = {
+    HOST,
+    USER,
+    PASSWORD,
+    DB,
+    dialect,
+    pool
+}
